e8131f00e765542d57f9be42f1e4048a5bff81b2
Filip <f.danisko@gmail.com>
2026-06-20 23:03:19 +0200

    Move OKF frontmatter parser types

--- packages/cli/internal/okf/frontmatter.go
+++ packages/cli/internal/okf/frontmatter.go
@@ -5,28 +5,6 @@ import (
 	"strings"
 )
 
-type frontmatter struct {
-	has      bool
-	values   map[string]string
-	keys     map[string]struct{}
-	warnings []frontmatterWarning
-	bodyLine int
-}
-
-type frontmatterWarning struct {
-	line    int
-	message string
-}
-
-type frontmatterParseError struct {
-	line    int
-	message string
-}
-
-func (e frontmatterParseError) Error() string {
-	return e.message
-}
-
 func splitFrontmatter(text string) (frontmatter, string, error) {
 	normalized := strings.ReplaceAll(text, "\r\n", "\n")
 	lines := strings.Split(normalized, "\n")
