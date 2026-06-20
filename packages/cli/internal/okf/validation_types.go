82d4be9b2d7ddb7c5b9726e2dbabebb252a3ed12
Filip <f.danisko@gmail.com>
2026-06-20 22:32:11 +0200

    Move OKF validation result types

---
+++ packages/cli/internal/okf/validation_types.go
@@ -0,0 +1,35 @@
+package okf
+
+import "fmt"
+
+type Issue struct {
+	Path    string `json:"path"`
+	Line    int    `json:"line,omitempty"`
+	Rule    string `json:"rule,omitempty"`
+	Message string `json:"message"`
+}
+
+func (i Issue) String() string {
+	if i.Line > 0 {
+		return fmt.Sprintf("%s:%d: %s", i.Path, i.Line, i.Message)
+	}
+	return fmt.Sprintf("%s: %s", i.Path, i.Message)
+}
+
+type Result struct {
+	Root        string  `json:"root"`
+	SpecVersion string  `json:"specVersion"`
+	Files       int     `json:"files"`
+	Concepts    int     `json:"concepts"`
+	Indexes     int     `json:"indexes"`
+	Logs        int     `json:"logs"`
+	Checks      []Check `json:"checks"`
+	Errors      []Issue `json:"errors"`
+	Warnings    []Issue `json:"warnings"`
+}
+
+type Check struct {
+	Name    string `json:"name"`
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
