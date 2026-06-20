f00b55de46ccb4d38e30354fe8a103bd5f81b385
Filip <f.danisko@gmail.com>
2026-06-20 22:18:18 +0200

    Move OKF AST diagnostic projection

--- packages/cli/internal/okf/ast_parse.go
+++ packages/cli/internal/okf/ast_parse.go
@@ -1,13 +1,11 @@
 package okf
 
 import (
-	"errors"
 	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
 	"sort"
-	"unicode/utf8"
 )
 
 func parseBundleAST(root string, version string) (astBundle, error) {
@@ -105,55 +103,4 @@ func parseASTDocumentLinks(root string, document astDocument) astDocument {
 	}
 	document.Links = ExtractLinks(root, document.Rel, document.Content)
 	return document
-}
-
-func astReadDiagnostic(err error) *astDiagnostic {
-	if err == nil {
-		return nil
-	}
-	return &astDiagnostic{
-		Message: err.Error(),
-		Cause:   err,
-	}
-}
-
-func astUTF8Diagnostic(content []byte) *astDiagnostic {
-	if utf8.Valid(content) {
-		return nil
-	}
-	return &astDiagnostic{
-		Line:    invalidUTF8Line(content),
-		Message: "Markdown file must be valid UTF-8",
-	}
-}
-
-func invalidUTF8Line(content []byte) int {
-	line := 1
-	for len(content) > 0 {
-		r, size := utf8.DecodeRune(content)
-		if r == utf8.RuneError && size == 1 {
-			return line
-		}
-		if content[0] == '\n' {
-			line++
-		}
-		content = content[size:]
-	}
-	return line
-}
-
-func astFrontmatterDiagnostic(err error) *astDiagnostic {
-	if err == nil {
-		return nil
-	}
-
-	line := 1
-	var parseErr frontmatterParseError
-	if errors.As(err, &parseErr) && parseErr.line > 0 {
-		line = parseErr.line
-	}
-	return &astDiagnostic{
-		Line:    line,
-		Message: err.Error(),
-	}
 }
